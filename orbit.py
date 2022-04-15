081ca26275696f434fc0f921af18f67ffd8bdb7f
jongwon254 <jongwon254@gmail.com>
2022-04-15 17:33:48 +0200

    orbiting planets

--- orbit.py
+++ orbit.py
@@ -50,6 +50,50 @@ class Planet:
         y = self.y * self.SCALE + HEIGHT / 2
         pygame.draw.circle(win, self.color, (x, y), self.radius)
 
+    # moving planets
+    def attraction(self, other):
+        other_x, other_y = other.x, other.y
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+
+        if other.sun:
+            self.distance_to_sun = distance
+        
+        # straight force
+        force = self.G * self.mass * other.mass / distance**2
+
+        # angle
+        theta = math.atan2(distance_y, distance_x)
+
+        # x,y force
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
+
+        return force_x, force_y
+
+    # forces of other planets
+    def update_position(self, planets):
+        total_fx = total_fy = 0
+
+        for planet in planets:
+            if self == planet:
+                continue
+
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+
+        # velocity F = m / a -> a = F / m
+        self.x_vel += total_fx / self.mass * self.TIMESTEP
+        self.y_vel += total_fy / self.mass * self.TIMESTEP
+
+        # update with velocity
+        self.x += self.x_vel * self.TIMESTEP
+        self.y += self.y_vel * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
+
+
 
 
 # main loop
@@ -61,12 +105,16 @@ def main():
     sun.sun = True  
 
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+    earth.y_vel = 29.783 * 1000
 
     mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+    mars.y_vel = 24.077 * 1000
 
     mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
+    mercury.y_vel = -47.4 * 1000
 
     venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+    venus.y_vel = -35.02 * 1000
 
     planets = [sun, earth, mars, mercury, venus]
 
@@ -85,6 +133,7 @@ def main():
 
         # draw planets
         for planet in planets:
+            planet.update_position(planets)
             planet.draw(WIN)
 
         pygame.display.update()
